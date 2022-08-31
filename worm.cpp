f4de1b44a6023feaab8a3599bfdc7b49308bd6f1
IanSanchezBaca <iansanchezbaca@gmail.com>
2022-08-31 15:19:33 -0700

    making the print body function

--- worm.cpp
+++ worm.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 #include<stdlib.h>
 using namespace std;
 
@@ -8,34 +9,45 @@ using namespace std;
 class Worm{
   private:
     int size;
-    bool dead = false;
-    // position x,y
+    bool dead = false; 
+    int Xpos = 1;
+    int Ypos = 1;
+    vector<int> body;
+    // not sure what im going to do with this vector yet
 
   public:
     bool is_dead();
     void print_field();
-    int testing = 10;
+    void print_body(int x, int y);
 
 }; // class worm
 
 bool Worm::is_dead(){
   return dead;
-}
+} // is_dead
 
+void Worm::print_body(int x, int y){
+  if(Xpos == x && Ypos == y){
+    cout << "Q";
+  }
+  else{
+    cout << " ";
+  }
+}// print_body
 
 void Worm::print_field(){
   for(int y = 0; y < 24; y++){
-    for(int x=0; x < 64; x++){ 
+    for(int x =0; x < 64; x++){ 
       if(y == 0 || y == 23 || x == 0 || x == 63){ 
         cout << "*";
       }
       else{
-        cout << "X";
+        print_body(x, y);
       }
     }
     cout << endl;
   }
-}
+} // print_field()
 
 
 
@@ -43,7 +55,7 @@ int main(){
   Worm wiggler; 
   while(!wiggler.is_dead()){
     system("clear");
-    //take the input first
+    //input
 
     //logic
 
