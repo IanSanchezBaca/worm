c192eb62bb596bb1c0e57396df1881f35bfe2571
IanSanchezBaca <iansanchezbaca@gmail.com>
2022-09-01 10:04:30 -0700

    hello world

--- update.bash
+++ update.bash
@@ -2,11 +2,22 @@ if [ $# -eq 0 ]
 then
 	echo "No argument given"
 	echo "please pass a comment as an argument"
-exit
+	exit
 fi
 git add .
 
-git commit -m "$1"
 
 git push origin main
 
+#############################
+bruh=""
+for i;
+do
+	bruh="$bruh $i"
+done
+#############################
+
+git commit -m "$bruh"
+
+
+git push origin main
