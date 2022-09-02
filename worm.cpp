c76c6bebf967ae2c34f67ce9e4a05387290adee2
IanSanchezBaca <iansanchezbaca@gmail.com>
2022-09-01 18:36:44 -0700

    need to learn how to make it print a bit faster

--- worm.cpp
+++ worm.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<random>
 #include<vector>
 #include<stdlib.h>
 #include<conio.h>
+#include <cstdlib>
 using namespace std;
 
 // input 
@@ -26,9 +28,16 @@ class Worm{
 
 }; // class worm
 class Food{
-  int xpos, ypos;
+  public:
+    int xpos = 0;
+    int ypos = 0;
+    void randomize();
+    bool ate = true;
 };
 
+void Food::randomize(){
+  //xpos = 
+}// randomize
 
 bool Worm::is_dead(){
   return dead;
@@ -44,41 +53,92 @@ void Worm::print_body(int x, int y){
 }// print_body
 
 void Worm::print_field(){
-  for(int y = 0; y < 24; y++){
-    for(int x =0; x < 64; x++){ 
-      if(y == 0 || y == 23 || x == 0 || x == 63){ 
-        cout << "*";
-      }
-      else{
-        print_body(x, y);
-      }
+  system("clear");
+  for(int y = 0; y < 20; y++){
+    for(int x =0; x < 20; x++){ 
+      switch (y) {
+        case 0:
+          cout << 'o';
+          break;
+
+        case 19: 
+          cout << 'o';
+          break;
+
+        default:
+          switch (x) {
+            case 0:
+              cout << 'o';
+              break;
+
+            case 19:
+              cout << 'o';
+              break;
+            default:
+              print_body(x, y); 
+              //cout << ' '; 
+              break;
+          }
+
+          break;
+      } 
+      //if(y == 0 || y == 23 || x == 0 || x == 63){ 
+      //cout << "o";
+      //}
+      //else{
+      //print_body(x, y);
+      //}
     }
     cout << endl;
   }
-} // print_field()
+} // print_field
 
 void Worm::logic(int input){
-  if(Xpos == 0 || Xpos == 64 || Ypos == 0 || Ypos == 24){
-    dead = true;
-    return;
+  switch(Xpos){
+    case 0:
+      dead = true;
+      return;
+    case 19:
+      dead = true;
+      return;
   }
-
-  if(input == 119 || input == 87){ // up
-    //cout << 'u' << endl;
-    direction = 'u';
+  switch(Ypos){
+    case 0:
+      dead = true;
+      return;
+    case 19:
+      dead = true;
+      return;
   }
-  if(input == 97 || input == 65){ // left
-    //cout << 'l' << endl;
-    direction = 'l';
-  }
-  if(input == 115 || input == 83){ // down
-    //cout << 'd' << endl;
-    direction = 'd';
-
-  }
-  if(input == 100 || input == 68){ // right
-    //cout << 'r' << endl;
-    direction = 'r';
+  //if(Xpos == 0 || Xpos == 64 || Ypos == 0 || Ypos == 23){
+  //dead = true;
+  //return;
+  //}
+  switch (input) {
+    case 119:
+      direction = 'u';
+      break;
+    case 87:
+      direction = 'u';
+      break;
+    case 97:
+      direction = 'l';
+      break;
+    case 65:
+      direction = 'l';
+      break;
+    case 115:
+      direction = 'd';
+      break;
+    case 83:
+      direction = 'd';
+      break;
+    case 100:
+      direction = 'r';
+      break;
+    case 68:
+      direction = 'r';
+      break;
   }
 
   if(direction == 'r'){
@@ -99,17 +159,21 @@ void Worm::logic(int input){
 
 int main(){
   Worm wiggler; 
+  Food apple; 
   while(!wiggler.is_dead()){
-    system("clear"); // clears the screen
     //input
+    system("clear"); // clears the screen
 
     int input = 0;
 
-    if(_kbhit()){
-      input = getch();
-    }
+    if(_kbhit()){ input = getch(); }
 
     //logic
+    if(apple.ate == true){
+      apple.ate = false; 
+      apple.randomize();
+    }
+
     wiggler.logic(input);
 
     //draw field
